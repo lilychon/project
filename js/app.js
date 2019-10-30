b2986cef59f72ffb659b7d099bbb2a17fc9c1d1a
Lily Chon <LChon@Perkinscoie.com>
2019-10-30 08:43:54 -0700

    Added project 1 files for testing

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $('body').css('display', 'none').fadeIn(1000);
+})
+
+$("#goBtn").on("click", function (event) {
+    event.preventDefault();
+    displayMapContent();
+    // var city = $("#destination").val();
+    getWeatherInfo("seattle");
+});
+
+function displayMapContent() {
+    $(".grid-container").empty();
+
+    $("<div>", {
+        html: "hello",
+        appendTo: ".map"
+    })
+
+    $("<iframe>", {
+        src: "https://maps.google.com/maps?q=seattle&t=&z=13&ie=UTF8&iwloc=&output=embed",
+        class: "framedMap",
+        appendTo: ".map"
+    })
+}
